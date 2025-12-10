5bc6b2c47c82ab68b7ba9ab9ea75e1cd2f8ee6e1
Makara <makarakara79@gmail.com>
2025-12-10 16:15:54 +0700

    [development] Auto commit on 2025-12-10 16:15:53 | Files: src/main/java/com/emenu/features/product/dto/request/AllProductPromotionRequest.java src/main/java/com/emenu/features/product/dto/request/AllProductRequest.java src/main/java/com/emenu/features/product/dto/request/AllSubCategoryRequest.java

--- src/main/java/com/emenu/features/product/dto/request/AllProductRequest.java
+++ src/main/java/com/emenu/features/product/dto/request/AllProductRequest.java
@@ -1,13 +1,17 @@
 package com.emenu.features.product.dto.request;
 
 import com.emenu.enums.common.Status;
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.util.UUID;
 
 @Builder
 @Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class AllProductRequest {
     @Builder.Default
     private int pageNo = 1;
--- src/main/java/com/emenu/features/product/dto/request/AllSubCategoryRequest.java
+++ src/main/java/com/emenu/features/product/dto/request/AllSubCategoryRequest.java
@@ -1,13 +1,17 @@
 package com.emenu.features.product.dto.request;
 
 import com.emenu.enums.common.Status;
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.util.UUID;
 
 @Builder
 @Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class AllSubCategoryRequest {
     @Builder.Default
     private int pageNo = 1;
